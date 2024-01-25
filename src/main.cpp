d09d3f31b35c81ce5586c0e2c6c7405f19ddd07c
dmj17b <dmj17b@my.fsu.edu>
2024-01-25 09:11:03 -0500

    First commit, working bird

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+
+#define LEDPIN 5
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+  pinMode(LEDPIN,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LEDPIN,HIGH);
+  delay(100);
+  digitalWrite(LEDPIN,LOW);
+  delay(100);
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
